c96ac9be738b9fc87bdaf0671eb3ea93a1f70f44
David <david.joseph.hynes@gmail.com>
2018-10-06 21:47:05 -0400

    refactoring for multiple projects

---
+++ FluentNHibernate/SessionFactories/SessionFactoryBuilder.cs
@@ -0,0 +1,14 @@
+using System;
+using NHibernate;
+using FluentNHibernate.Cfg;
+
+namespace ORMTest.SessionFactories
+{
+    public class SessionFactoryBuilder
+    {
+        // public static ISessionFactory BuildSessionFactory(string connectionStringName, bool create = false, bool update = false)
+        // {
+            
+        // }
+    }
+}
